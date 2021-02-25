664081af0dacf031a9fcc069abb010df2e93e2ce
evansmwenda <evansmwenda.em@gmail.com>
2021-02-25 16:42:12 +0300

    added add expense ui

---
+++ lib/add_expense.dart
@@ -0,0 +1,335 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AddExpense extends StatefulWidget {
+  static const routeName = '/add-expense';
+  @override
+  _AddExpenseState createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  final _formKey = GlobalKey<FormState>();
+  DateTime selectedDate = DateTime.now();
+  var myFormat = DateFormat('d-MM-yyyy');
+
+  int _value = 1;
+  String _chosenValueAir;
+
+  final myController = TextEditingController();
+  final name = "Name";
+  // DateTime selectedDate =  DateFormat("dd-MM-yyyy").format(DateTime.now());
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2020, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _datecontroller = new TextEditingController(text: '${myFormat.format(selectedDate)}');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Expense"),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 25.0, right: 25.0, top: 5.0),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                SizedBox(height: 30.0),
+                Container(
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          width: 1.0,
+                          style: BorderStyle.solid,
+                          color: Colors.blue),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      focusColor: Colors.green,
+                      value: _chosenValueAir,
+                      //elevation: 5,
+                      style: TextStyle(color: Colors.white),
+                      iconEnabledColor: Colors.blue,
+                      items: <String>[
+                        'Fare',
+                        'Food',
+                        'Flutter',
+                        'Node',
+                        'Java',
+                        'Python',
+                        'PHP',
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 5.0),
+                            child: Text(
+                              value,
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      hint: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Text(
+                          "Select Category",
+                          style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      onChanged: (String value) {
+                        setState(() {
+                          _chosenValueAir = value;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Container(
+                  height: 50,
+                  // margin: EdgeInsets.all(
+                  //   10.0,
+                  // ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[200],
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      TextFormField(
+                        cursorColor: Colors.red,
+                        decoration: InputDecoration(
+                          labelText: "Expense Name",
+                          contentPadding: EdgeInsets.symmetric(
+                            // vertical: 5.0,
+                            horizontal: 5.0,
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Enter expense name';
+                          }
+                        },
+                      ),
+                      Positioned(
+                        bottom: 1,
+                        child: Container(
+                          height: 1,
+                          width: MediaQuery.of(context).size.width - 20,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color(0xffff0000),
+                                const Color(0xff6b0000),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  height: 50,
+                  // margin: EdgeInsets.all(
+                  //   10.0,
+                  // ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[200],
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      TextFormField(
+                        cursorColor: Colors.red,
+                        decoration: InputDecoration(
+                          labelText: "Expense Amount",
+                          contentPadding: EdgeInsets.symmetric(
+                            // vertical: 5.0,
+                            horizontal: 5.0,
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Enter expense amount';
+                          }
+                        },
+                      ),
+                      Positioned(
+                        bottom: 1,
+                        child: Container(
+                          height: 1,
+                          width: MediaQuery.of(context).size.width - 20,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color(0xffff0000),
+                                const Color(0xff6b0000),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                GestureDetector(
+                  onTap: () => _selectDate(context),
+                  child: Container(
+                    height: 50,
+                    // margin: EdgeInsets.all(
+                    //   10.0,
+                    // ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.grey[200],
+                    ),
+                    child: Stack(
+                      children: <Widget>[
+                        IgnorePointer(
+                          child: TextFormField(
+                            cursorColor: Colors.red,
+                            controller: _datecontroller,
+                            decoration: InputDecoration(
+                              labelText: "Expense Date",
+                              contentPadding: EdgeInsets.symmetric(
+                                // vertical: 5.0,
+                                horizontal: 5.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 1,
+                          child: Container(
+                            height: 1,
+                            width: MediaQuery.of(context).size.width - 20,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  const Color(0xffff0000),
+                                  const Color(0xff6b0000),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  // height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[200],
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      TextFormField(
+                        maxLines: 5,
+                        cursorColor: Colors.red,
+                        decoration: InputDecoration(
+                          labelText: "Any Additional details",
+                          contentPadding: EdgeInsets.symmetric(
+                            // vertical: 5.0,
+                            horizontal: 5.0,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 1,
+                        child: Container(
+                          height: 1,
+                          width: MediaQuery.of(context).size.width - 20,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color(0xffff0000),
+                                const Color(0xff6b0000),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 50.0),
+                Container(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.blue),
+                      ),
+                      color: Colors.blue[700],
+                      textColor: Colors.white,
+                      // padding: EdgeInsets.symmetric(
+                      //     vertical: 12.0, horizontal: 40.0),
+                      onPressed: () {},
+                      child: SizedBox(
+                        width: 107,
+                        height: 39,
+                        child: Center(
+                          child: Text(
+                            "Continue",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Expense {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Expense.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Expense.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), reference: snapshot.reference);
+
+  void save() => print("Record<$name:$votes>");
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expnsr/add_expense.dart';
+import 'package:expnsr/names.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 // void main() => runApp(MyApp());
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      // This makes the visual density adapt to the platform that you run
+      // the app on. For desktop platforms, the controls will be smaller and
+      // closer together (more dense) than on mobile platforms.
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MyApp(),//MyApp(),
+      AddExpense.routeName: (context) => AddExpense(),//AddExpense page
+      Names.routeName: (context) => Names(),//Names page
+    },
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Baby Names',
-      home: MyHomePage(),
-    );
+    return MyHomePage();
   }
 }
 
@@ -30,8 +44,82 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
+      appBar: AppBar(title: Text('Dashboard')),
       body: _buildBody(context),
+      floatingActionButton: _myCustomFloatingActionButton(context),
+      // FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.pushNamed(context, AddExpense.routeName);
+      //   },
+      //   tooltip: "Add Expense",
+      //   child: new Icon(Icons.add),
+      // ),
+    );
+  }
+
+  Widget _myCustomFloatingActionButton(BuildContext context){
+    return SpeedDial(
+      /// both default to 16
+      marginEnd: 18,
+      marginBottom: 20,
+      animatedIcon: AnimatedIcons.menu_close,
+      // animatedIconTheme: IconThemeData(size: 22.0),
+      /// This is ignored if animatedIcon is non null
+      // icon: Icons.add,
+      // activeIcon: Icons.remove,
+      // iconTheme: IconThemeData(color: Colors.grey[50], size: 30),
+      /// The label of the main button.
+      // label: Text("Open Speed Dial"),
+      /// The active label of the main button, Defaults to label if not specified.
+      // activeLabel: Text("Close Speed Dial"),
+      /// Transition Builder between label and activeLabel, defaults to FadeTransition.
+      // labelTransitionBuilder: (widget, animation) => ScaleTransition(scale: animation,child: widget),
+      /// The below button size defaults to 56 itself, its the FAB size + It also affects relative padding and other elements
+      buttonSize: 56.0,
+      visible: true,
+      /// If true user is forced to close dial manually
+      /// by tapping main button and overlay is not rendered.
+      closeManually: false,
+      /// If true overlay will render no matter what.
+      renderOverlay: false,
+      curve: Curves.bounceIn,
+      overlayColor: Colors.black,
+      overlayOpacity: 0.5,
+      onOpen: () => print('OPENING DIAL'),
+      onClose: () => print('DIAL CLOSED'),
+      tooltip: 'Speed Dial',
+      heroTag: 'speed-dial-hero-tag',
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      elevation: 8.0,
+      shape: CircleBorder(),
+      // orientation: SpeedDialOrientation.Up,
+      // childMarginBottom: 2,
+      // childMarginTop: 2,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.accessibility),
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white,
+          label: 'Names',
+          labelStyle: TextStyle(fontSize: 18.0),
+          onTap: () {
+            Navigator.pushNamed(context, Names.routeName);
+          },
+          onLongPress: () => print('FIRST CHILD LONG PRESS'),
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          label: 'Expense',
+          labelStyle: TextStyle(fontSize: 18.0),
+          onTap: () {
+            Navigator.pushNamed(context, AddExpense.routeName);
+          },
+          onLongPress: () => print('SECOND CHILD LONG PRESS'),
+        ),
+      ],
     );
   }
 
